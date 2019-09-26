f7fa21720e06a156ca99852029db54c8d27c5042
micheal <micheal.cusson@dcmail.ca>
2019-09-26 14:10:34 -0400

    Edited by Mike Cusson

--- welcome.php
+++ welcome.php
@@ -6,6 +6,7 @@ LAST MODIFIED   : SEPT 15, 2019
 DESCRIPTION     : Welcome Page
 -->
 <?php
+
 $title  = "Welcome";
 $file   = "Welcome.php";
 $date   = "SEPT 15, 2019";
@@ -13,13 +14,6 @@ $banner = "Welcome";
 $desc   = "Welcome page use to greet clients.";
 require("./header.php");
 
-print_r($_COOKIE);
-
-print("<br/><br/>");
-$cookie_currentUser = explode("|",$_COOKIE['LOGIN_COOKIE']);
-print_r($cookie_currentUser);
-print("<br/><br/>");
-print_r($cookie_currentUser[1]);
 ?>
     <div>
         Welcome

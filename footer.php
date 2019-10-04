88180698b11c2305d02d4bc6dde7588f5416bbd5
Smit Patel <smitbharatkumar.patel@dcmail.ca>
2019-10-03 20:51:36 -0400

    Edited by Mike Cusson

--- footer.php
+++ footer.php
@@ -37,9 +37,8 @@ DESCRIPTION     : Footer section
         <div class="w-full flex flex-wrap py-4 justify-between">
             <?php display_copyright() ?>
             <p>
-                <a href="http://validator.w3.org/check?uri=referer%22%3E">
-                    <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
-                </a>
+                <a href="http://validator.w3.org/check?uri=referer"><img
+                src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
             </p>
         </div> 
     </div>
